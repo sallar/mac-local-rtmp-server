33d78c769ce0bd6afa9b0e8a78304ad46685db86
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-04-08 22:28:59 +0300

    Show streams info

--- main.js
+++ main.js
@@ -10,20 +10,25 @@ const menubar = Menubar({
   height: 200
 });
 
+const nms = new NodeMediaServer({
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+});
+nms.run();
+
 menubar.on('ready', () => {
-  const config = {
-    rtmp: {
-      port: 1935,
-      chunk_size: 60000,
-      gop_cache: true,
-      ping: 60,
-      ping_timeout: 30
-    },
-    http: {
-      port: 8000,
-      allow_origin: '*'
-    }
-  };
-  var nms = new NodeMediaServer(config);
-  nms.run();
+  menubar.showWindow();
+});
+
+menubar.on('after-create-window', () => {
+  menubar.window.webContents.openDevTools();
 });
--- renderer.js
+++ renderer.js
@@ -1,1 +1,29 @@
-console.log('Hello');
+const { ipcRenderer } = require('electron');
+const template = require('lodash/template');
+const fs = require('fs');
+const filesize = require('filesize');
+
+const API_URI = 'http://localhost:8000/api';
+
+const streamsTemplate = template(fs.readFileSync('./view/streams.ejs', 'utf8'));
+const streamsContainer = document.getElementById('streams');
+
+function fetchStreamInfo() {
+  fetch(`${API_URI}/streams`)
+    .then(res => res.json())
+    .then(res => {
+      if (typeof res !== 'object' || !('live' in res)) {
+        return;
+      }
+      streamsContainer.innerHTML = streamsTemplate(
+        Object.assign({}, res, {
+          tools: {
+            filesize
+          }
+        })
+      );
+    });
+}
+
+fetchStreamInfo();
+setInterval(fetchStreamInfo, 5000);

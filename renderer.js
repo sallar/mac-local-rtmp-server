36592d11d81d3e7b5bc357a18d6b3b7175419a7d
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-04-09 20:36:52 +0300

    Remove test code

--- renderer.js
+++ renderer.js
@@ -13,55 +13,18 @@ const streamsTemplate = template(
 const streamsContainer = document.getElementById('streams');
 
 function fetchStreamInfo() {
-  // fetch(`${API_URI}/streams`)
-  //   .then(res => res.json())
-  //   .then(res => {
-  //     streamsContainer.innerHTML = streamsTemplate(
-  //       Object.assign({}, res, {
-  //         rtmpUri,
-  //         tools: {
-  //           filesize
-  //         }
-  //       })
-  //     );
-  //   });
-  const res = {
-    live: {
-      sallar: {
-        publisher: {
-          app: 'live',
-          stream: 'sallar',
-          clientId: '94SL1RVD',
-          connectCreated: '2018-04-09T17:25:29.064Z',
-          bytes: 4807026,
-          ip: '127.0.0.1',
-          audio: {
-            codec: 'AAC',
-            profile: 'LC',
-            samplerate: 44100,
-            channels: 2
-          },
-          video: {
-            codec: 'H264',
-            width: 1920,
-            height: 1080,
-            profile: 'High',
-            level: 4.2,
-            fps: 60
+  fetch(`${API_URI}/streams`)
+    .then(res => res.json())
+    .then(res => {
+      streamsContainer.innerHTML = streamsTemplate(
+        Object.assign({}, res, {
+          rtmpUri,
+          tools: {
+            filesize
           }
-        },
-        subscribers: []
-      }
-    }
-  };
-  streamsContainer.innerHTML = streamsTemplate(
-    Object.assign({}, res, {
-      rtmpUri,
-      tools: {
-        filesize
-      }
-    })
-  );
+        })
+      );
+    });
 }
 
 fetchStreamInfo();
